8ca7bb3ebc63d8ab31a76f11c519013ecb2a55d1
James Warren <mail@jamesjwarren.com>
2015-02-26 11:58:23 +0000

    cache animation elements to avoid DOM lookups on every animation

--- app/js/directives/velocity-button.js
+++ app/js/directives/velocity-button.js
@@ -50,13 +50,39 @@ angular.module("sn.velocity.snVelocityButton", [
                 $scope.toggleState = false;
 
                 /**
+                 * Cache animation elements
+                 * @property {Object} animatedElements
+                 */
+                $scope.animatedElements = {
+
+                    /**
+                     * Cached DOM elements for animation
+                     * @property {Object} get
+                     */
+                    get: {},
+
+                    /**
+                     * Set animated element cache
+                     * @param {Object} elementKeyframes animation keyframes object from attribute
+                     */
+                    set: function animatedElementsSetter(elementKeyframes) {
+                        angular.forEach(elementKeyframes, function(keyframes, key){
+                            var animationElement = $element[0].querySelector(key);
+                            $scope.animatedElements.get[key] = animationElement;
+                        });
+                    }
+                };
+
+                $scope.animatedElements.set($scope.onClickOnKeyframes || $scope.onClickOffKeyframes || $scope.onMouseEnterKeyframes || $scope.onMouseLeaveKeyframes);
+
+                /**
                  * Run animation on child elements
                  * @param {Object} elementKeyframes   object of elements with keyframe arrays
                  */
                 $scope.animate = function animate(elementKeyframes){
 
                     angular.forEach(elementKeyframes, function(keyframes, key){
-                        var animationElement = $element[0].querySelector(key);
+                        var animationElement = $scope.animatedElements.get[key];
                         angular.forEach(keyframes, function(value){
                             $window.Velocity(animationElement, value.properties, value.options);
                         });
@@ -74,7 +100,7 @@ angular.module("sn.velocity.snVelocityButton", [
                 $scope.onClick = function onClick() {
                     if ($scope.toggleState) {
                         // stop current animation
-                        angular.forEach($scope.animatedElements, function(animatedElement){
+                        angular.forEach($scope.animatedElements.get, function(animatedElement){
                             $window.Velocity(animatedElement, "stop");
                         });
 
@@ -101,7 +127,8 @@ angular.module("sn.velocity.snVelocityButton", [
                  */
                 $scope.onMouseLeave = function onMouseLeave() {
                     // stop current animation
-                    angular.forEach($scope.animatedElements, function(animatedElement){
+                    angular.forEach($scope.animatedElements.get, function(animatedElement){
+                        console.log(animatedElement);
                         $window.Velocity(animatedElement, "stop");
                     });
 
